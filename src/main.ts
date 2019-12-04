16071227451467ff90c79b8285a905a494166eee
michael-spengler <>
2019-12-04 15:46:35 +0100

    flexible port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigurationReader } from 'configuration-reader';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(80);
+
+// you can adjust the filename here - remember updating .gitignore
+  const yourFileName: string = '../.env';
+
+  const configurationReader: ConfigurationReader =
+    new ConfigurationReader(path.join(__dirname, yourFileName));
+
+  const portComingFromConfigFile: string = configurationReader.get('port');
+
+  const portNumber: string = portComingFromConfigFile;
+
+  await app.listen(portNumber);
+
 }
 bootstrap();
